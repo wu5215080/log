e0c59137964b2c481da1945dc6ba02e68ff6125a
Abu <abu.wu@dingtone.me>
2019-10-14 20:08:36 +0800

    时间精确到ns

--- logger.go
+++ logger.go
@@ -31,7 +31,7 @@ const (
 
 var LevelName [6]string = [6]string{"Trace", "Debug", "Info", "Warn", "Error", "Fatal"}
 
-const TimeFormat = "2006-01-02 15:04:05:010"
+const TimeFormat = "2006-01-02 15:04:05.999999999"
 
 const maxBufPoolSize = 16
 
